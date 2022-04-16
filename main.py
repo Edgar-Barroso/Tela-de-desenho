7b8bdaf39d4cf19124f0e42b4094b9a61978ad61
Jose Edgar Barroso Neto <96843649+Edgar-Barroso@users.noreply.github.com>
2022-04-15 21:40:55 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+import pygame
+import random
+import cv2
+import numpy as np
+import torch
+from torch import nn, optim
+from torchvision import datasets
+from torchvision import transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import time
+
+largura = 800
+altura = 600
+frames = 120
+cor_branca = (255, 255, 255)
+cor_pincel = (80, 11, 200)
+
+
+def main(frames):
+    pygame.init()
+    tela = pygame.display.set_mode([largura, altura])
+    tela.fill(cor_branca)
+    relogio = pygame.time.Clock()
+    sair = False
+    while sair is False:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pintar = True
+                while pintar:
+                    pygame.display.update()
+                    x, y = pygame.mouse.get_pos()
+                    pygame.draw.circle(tela, (random.random() * 255, random.random() * 255, random.random() * 255),(x, y), 8)
+                    for event2 in pygame.event.get():
+                        if event2.type == pygame.MOUSEBUTTONUP:
+                            pintar = False
+                            break
+                break
+            if event.type == pygame.QUIT:
+                sair = True
+                break
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    tela = pygame.display.set_mode([largura, altura])
+                    tela.fill(cor_branca)
+        relogio.tick(frames)
+        pygame.display.update()
+    pygame.image.save(tela, 'tela.jpeg')
+    pygame.quit()
+    # img = cv2.imread('tela.jpeg')
+    # res = cv2.resize(img, dsize=(30, 30), interpolation=cv2.INTER_CUBIC)
+    # cv2.imwrite('imagem.jpeg',res)
+
+
+if __name__ == '__main__':
+    main(frames)
